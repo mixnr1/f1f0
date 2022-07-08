a2500d921cb877df1d03b1ae17ea4b3ba67317a9
mixnr1 <38935530+mixnr1@users.noreply.github.com>
2022-07-08 16:51:37 +0300

    Add files via upload

--- f1f0.py
+++ f1f0.py
@@ -5,7 +5,7 @@ import pandas as pd
 def atlikums_aprekinasana(nested_list, list):
     if list[3]>0: 
         nested_list.append([list[0], list[1], list[2], list[3]])
-        print(nested_list)
+        # print(nested_list)
     else:
         for i in range(len(nested_list)):
             if (list[3]<0)==True and ((nested_list[i][3]+list[3])>0)==True: 
@@ -24,7 +24,16 @@ def atlikums_aprekinasana(nested_list, list):
                 nested_list[i][1]=atlikuma_vertiba_no_pardosanas #'EUR'
                 nested_list[i][3]=atlikums_no_pardosanas #'BTC'
                 # print("FINAL Pārdoti", abs(pardotie_btc), "BTC, kas iegādāti", iegades_datums, "par", iegades_kurss, "EUR (",round(iegades_vertiba,2),"EUR ). Pardosanas kurss (",pardosanas_datums,"):", pardosanas_kurss, "EUR/BTC; Pārdošanas vērtība:", round(pardosanas_vertiba, 2), "EUR. Peļņa:",pelna, "EUR. Nodoklis:",nodoklis, "EUR")
-                print(nested_list, "FINAL SELL", abs(pardotie_btc),"| BUY_date",iegades_datums,"| BUY_rate",iegades_kurss,"| BUY_value",round(iegades_vertiba,2),"| SELL_date",pardosanas_datums,"| SELL_rate", pardosanas_kurss, "| SELL_value", round(pardosanas_vertiba, 2), "| Profit",pelna, "| TAX",nodoklis)
+                # print(nested_list, "FINAL SELL", abs(pardotie_btc),"| BUY_date",iegades_datums,"| BUY_rate",iegades_kurss,"| BUY_value",round(iegades_vertiba,2),"| SELL_date",pardosanas_datums,"| SELL_rate", pardosanas_kurss, "| SELL_value", round(pardosanas_vertiba, 2), "| Profit",pelna, "| TAX",nodoklis)
+                print("FINAL SELL_date",pardosanas_datums,\
+                "|SELL_rate", pardosanas_kurss, \
+                "|SOLD_BTC", abs(pardotie_btc),\
+                "|SELL_value", round(pardosanas_vertiba, 2), \
+                "|BUY_date",iegades_datums,\
+                "|BUY_rate",iegades_kurss,\
+                "|BUY_value",round(iegades_vertiba,2),\
+                "|Profit",pelna)#, \
+                # "| TAX",nodoklis)
                 break
             elif (list[3]<0)==True and ((nested_list[i][3]+list[3])<0)==True:
                 iegades_kurss=nested_list[i][2]
@@ -45,8 +54,16 @@ def atlikums_aprekinasana(nested_list, list):
                 list[1]=atlikums_no_pardosanas_vertiba #'EUR'
                 list[3]=atlikums_no_pardosanas #'BTC'
                 # print("OUTGOING Pārdoti", pardotie_btc, "BTC, kas iegādāti", iegades_datums, "par", iegades_kurss, "EUR (",round(iegades_vertiba,2),"EUR ). Pardosanas kurss (",pardosanas_datums,"):", pardosanas_kurss, "EUR/BTC; Pārdošanas vērtība:", round(pardosanas_vertiba, 2), "EUR. Peļņa:",pelna, "EUR. Nodoklis:",nodoklis, "EUR")
-                print(nested_list, "OUTGI SELL", pardotie_btc, "| BUY_date", iegades_datums, "| BUY_rate", iegades_kurss, "| BUY_value",round(iegades_vertiba,2),"| SELL_date",pardosanas_datums,"| SELL_rate", pardosanas_kurss, "| SELL_value", round(pardosanas_vertiba, 2), "| Profit",pelna, "| TAX",nodoklis)
-                
+                # print(nested_list, "OUTGI SELL", pardotie_btc, "| BUY_date", iegades_datums, "| BUY_rate", iegades_kurss, "| BUY_value",round(iegades_vertiba,2),"| SELL_date",pardosanas_datums,"| SELL_rate", pardosanas_kurss, "| SELL_value", round(pardosanas_vertiba, 2), "| Profit",pelna, "| TAX",nodoklis)
+                print("OUTGI SELL_date", pardosanas_datums,\
+                "|SELL_rate", pardosanas_kurss,\
+                "|SOLD_BTC", pardotie_btc,\
+                "|SELL_value", round(pardosanas_vertiba, 2), \
+                "|BUY_date", iegades_datums, \
+                "|BUY_rate", iegades_kurss, \
+                "|BUY_value",round(iegades_vertiba,2),\
+                "|Profit",pelna)#, \
+                # "| TAX",nodoklis)
 
 file_path=config.file_path
 df = pd.read_excel(file_path)
@@ -54,5 +71,5 @@ fifo_list=[]
 for index, row in df.iterrows():
     atlikums_aprekinasana(fifo_list, [row['Datums'], row['EUR'], row['EUR/BTC'], row['BTC']])
 
-# for i in fifo_list:
-#     print(i)
+for i in fifo_list:
+    print(i)
