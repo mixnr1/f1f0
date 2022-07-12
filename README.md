633bbafcfd57a79caa1dcd0bb30ad0bc2da89d18
mixnr1 <38935530+mixnr1@users.noreply.github.com>
2022-07-12 10:33:27 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # f1f0
 FIFO calculator
+
+Usage example:
+python3 f1f0.py --file /home/user/Desktop/test.xlsx --column_names 'Datums' 'EUR' 'EUR/BTC' 'BTC' 
+python3 f1f0.py --file /home/user/Desktop/test.xlsx --column_names Datums EUR EUR/BTC BTC
+python3 f1fo.py -f /home/user/Desktop/test.xlsx -c Datums EUR EUR/BTC BTC
+python3 f1fo.py -f /home/user/Desktop/test.xlsx
--- f1f0.py
+++ f1f0.py
@@ -1,75 +1,75 @@
-from itertools import count
-import config
 import pandas as pd
+import argparse
+ 
 
 def atlikums_aprekinasana(nested_list, list):
     if list[3]>0: 
         nested_list.append([list[0], list[1], list[2], list[3]])
-        # print(nested_list)
     else:
         for i in range(len(nested_list)):
             if (list[3]<0)==True and ((nested_list[i][3]+list[3])>0)==True: 
                 iegades_kurss=nested_list[i][2]
                 iegades_datums=nested_list[i][0]
                 pardotie_btc=list[3]
-                iegades_vertiba=abs(round((pardotie_btc*iegades_kurss),2))
+                iegades_vertiba=abs((pardotie_btc*iegades_kurss))
                 atlikums_no_pardosanas=nested_list[i][3]+list[3]
-                atlikuma_vertiba_no_pardosanas=round((iegades_kurss*atlikums_no_pardosanas),2)
+                atlikuma_vertiba_no_pardosanas=(iegades_kurss*atlikums_no_pardosanas)
                 pardosanas_kurss=list[2]
                 pardosanas_datums=list[0]
-                pardosanas_vertiba=abs(round((pardotie_btc*pardosanas_kurss),2))
-                pelna=round((pardosanas_vertiba-iegades_vertiba),2)
-                nodoklis=round((pelna*0.2),2)
-                #updated_fields_after_transaction
-                nested_list[i][1]=atlikuma_vertiba_no_pardosanas #'EUR'
-                nested_list[i][3]=atlikums_no_pardosanas #'BTC'
-                # print("FINAL Pārdoti", abs(pardotie_btc), "BTC, kas iegādāti", iegades_datums, "par", iegades_kurss, "EUR (",round(iegades_vertiba,2),"EUR ). Pardosanas kurss (",pardosanas_datums,"):", pardosanas_kurss, "EUR/BTC; Pārdošanas vērtība:", round(pardosanas_vertiba, 2), "EUR. Peļņa:",pelna, "EUR. Nodoklis:",nodoklis, "EUR")
-                # print(nested_list, "FINAL SELL", abs(pardotie_btc),"| BUY_date",iegades_datums,"| BUY_rate",iegades_kurss,"| BUY_value",round(iegades_vertiba,2),"| SELL_date",pardosanas_datums,"| SELL_rate", pardosanas_kurss, "| SELL_value", round(pardosanas_vertiba, 2), "| Profit",pelna, "| TAX",nodoklis)
-                print("FINAL SELL_date",pardosanas_datums,\
-                "|SELL_rate", pardosanas_kurss, \
-                "|SOLD_BTC", abs(pardotie_btc),\
-                "|SELL_value", round(pardosanas_vertiba, 2), \
-                "|BUY_date",iegades_datums,\
-                "|BUY_rate",iegades_kurss,\
-                "|BUY_value",round(iegades_vertiba,2),\
-                "|Profit",pelna)#, \
-                # "| TAX",nodoklis)
+                pardosanas_vertiba=abs((pardotie_btc*pardosanas_kurss))
+                pelna=pardosanas_vertiba-iegades_vertiba
+                nodoklis=pelna*0.2
+                nested_list[i][1]=atlikuma_vertiba_no_pardosanas 
+                nested_list[i][3]=atlikums_no_pardosanas 
+                print(pardosanas_datums,\
+                ",", pardosanas_kurss, \
+                ",", abs(pardotie_btc),\
+                ",", pardosanas_vertiba, \
+                ",",iegades_datums,\
+                ",",iegades_kurss,\
+                ",",iegades_vertiba,\
+                ",",pelna, \
+                ",",nodoklis)
                 break
             elif (list[3]<0)==True and ((nested_list[i][3]+list[3])<0)==True:
                 iegades_kurss=nested_list[i][2]
                 iegades_datums=nested_list[i][0]
-                pardotie_btc=nested_list[i][3]#!!!!!!!!!!!!!!!!!!
-                iegades_vertiba=abs(round((pardotie_btc*iegades_kurss),2))
+                pardotie_btc=nested_list[i][3]
+                iegades_vertiba=abs((pardotie_btc*iegades_kurss))
                 atlikums_no_pardosanas=nested_list[i][3]+list[3]
-                atlikuma_vertiba_no_pardosanas=round((iegades_kurss*atlikums_no_pardosanas),2)
-                atlikums_no_pardosanas_vertiba=round((atlikums_no_pardosanas*list[2]),2)
+                atlikuma_vertiba_no_pardosanas=((iegades_kurss*atlikums_no_pardosanas))
+                atlikums_no_pardosanas_vertiba=atlikums_no_pardosanas*list[2]
                 pardosanas_kurss=list[2]
                 pardosanas_datums=list[0]
-                pardosanas_vertiba=abs(round((pardotie_btc*pardosanas_kurss),2)) 
-                pelna=round((pardosanas_vertiba-iegades_vertiba),2)
-                nodoklis=round((pelna*0.2),2)
-                #updated_fields_after_transaction
-                nested_list[i][1]=0 #'EUR'
-                nested_list[i][3]=0 #'BTC'
-                list[1]=atlikums_no_pardosanas_vertiba #'EUR'
-                list[3]=atlikums_no_pardosanas #'BTC'
-                # print("OUTGOING Pārdoti", pardotie_btc, "BTC, kas iegādāti", iegades_datums, "par", iegades_kurss, "EUR (",round(iegades_vertiba,2),"EUR ). Pardosanas kurss (",pardosanas_datums,"):", pardosanas_kurss, "EUR/BTC; Pārdošanas vērtība:", round(pardosanas_vertiba, 2), "EUR. Peļņa:",pelna, "EUR. Nodoklis:",nodoklis, "EUR")
-                # print(nested_list, "OUTGI SELL", pardotie_btc, "| BUY_date", iegades_datums, "| BUY_rate", iegades_kurss, "| BUY_value",round(iegades_vertiba,2),"| SELL_date",pardosanas_datums,"| SELL_rate", pardosanas_kurss, "| SELL_value", round(pardosanas_vertiba, 2), "| Profit",pelna, "| TAX",nodoklis)
-                print("OUTGI SELL_date", pardosanas_datums,\
-                "|SELL_rate", pardosanas_kurss,\
-                "|SOLD_BTC", pardotie_btc,\
-                "|SELL_value", round(pardosanas_vertiba, 2), \
-                "|BUY_date", iegades_datums, \
-                "|BUY_rate", iegades_kurss, \
-                "|BUY_value",round(iegades_vertiba,2),\
-                "|Profit",pelna)#, \
-                # "| TAX",nodoklis)
+                pardosanas_vertiba=abs(pardotie_btc*pardosanas_kurss)
+                pelna=pardosanas_vertiba-iegades_vertiba
+                nodoklis=pelna*0.2
+                nested_list[i][1]=0 
+                nested_list[i][3]=0 
+                list[1]=atlikums_no_pardosanas_vertiba 
+                list[3]=atlikums_no_pardosanas 
+                print(pardosanas_datums,\
+                ",", pardosanas_kurss,\
+                ",", pardotie_btc,\
+                ",", pardosanas_vertiba, \
+                ",", iegades_datums, \
+                ",", iegades_kurss, \
+                ",", iegades_vertiba,\
+                ",", pelna, \
+                ",", nodoklis)
 
-file_path=config.file_path
-df = pd.read_excel(file_path)
-fifo_list=[]
-for index, row in df.iterrows():
-    atlikums_aprekinasana(fifo_list, [row['Datums'], row['EUR'], row['EUR/BTC'], row['BTC']])
+def main(input_file, column_name):
+    file_path=input_file
+    df = pd.read_excel(file_path)
+    fifo_list=[]
+    print("sell_date, sell_price, sell_amount, sell_value, buy_date, buy_price, buy_value, profit, tax")
+    for index, row in df.iterrows():
+        atlikums_aprekinasana(fifo_list, [row[column_name[0]], row[column_name[1]], row[column_name[2]], row[column_name[3]]])
 
-for i in fifo_list:
-    print(i)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="FIFO calculator")
+    parser.add_argument("-f", "--file", help="path to input file")
+    parser.add_argument("-c", "--column_names", nargs='+', default=['Datums', 'EUR', 'EUR/BTC', 'BTC'])
+    args = parser.parse_args()
+    main(args.file, args.column_names)
+
